aa17bf4fb4c72a2b7006c423dbceafe57a244e95
Eliot Muir <eliotmuir@Eliots-MacBook-Pro-2.local>
2021-09-27 13:16:43 -0400

    Shifting sandboxes around.

--- COLtest/main.cpp
+++ COLtest/main.cpp
@@ -1,18 +1,7 @@
 #include <stdio.h>
-#include <lua/lua.hpp>  // using C++ header file for C linkage
-
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  // Open lua
-  lua_State *L = luaL_newstate();
-  // Load the libraries
-  luaL_openlibs(L);
-
-  // Execution of a lua string
-  luaL_dostring(L, "print \"The answer to life and universe and everything is Lua.\"");
-  // Close lua
-  lua_close (L);
-
+  printf("Hello!\n");
   return 0;
 }
