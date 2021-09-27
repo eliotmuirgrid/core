72788381f57a34393b05c6ea6c014bea1f5ca797
Eliot Muir <eliotmuir@Eliots-MacBook-Pro-2.local>
2021-09-27 13:07:24 -0400

    Simplified the example by using the Lua auxilary API.

--- COLtest/main.cpp
+++ COLtest/main.cpp
@@ -1,26 +1,11 @@
 #include <stdio.h>
-
-extern "C" {
-   #include <lua/lua.h>
-   #include <lua/lualib.h>
-   #include <lua/lauxlib.h>
-}
+#include <lua/lua.hpp>  // using C++ header file for C linkage
 
 #include <stdlib.h>
 
-static void* l_alloc (void* ud, void* ptr, size_t osize, size_t nsize) {
-    (void)ud;  (void)osize;  /* not used */
-    if (nsize == 0) {
-      free(ptr);
-      return NULL;
-    }
-    else return realloc(ptr, nsize);
-}
-
 int main(int argc, char *argv[]) {
   // Open lua
-  lua_State *L = lua_newstate(&l_alloc, NULL);
-
+  lua_State *L = luaL_newstate();
   // Load the libraries
   luaL_openlibs(L);
 
