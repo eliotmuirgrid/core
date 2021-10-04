0abc8db634e936d5733903e17ba5e32dc9c0ca34
Eliot Muir <eliot.muir@icloud.com>
2021-10-03 21:39:08 -0400

    Ported to windows.

--- BAS/BASutil.cpp
+++ BAS/BASutil.cpp
@@ -10,13 +10,21 @@
 #include "BAStrace.h"
 BAS_TRACE_INIT;
 
-#include <time.h>
+#ifdef _WIN32 
+   #include <windows.h>
+#else
+    #include <time.h>
+#endif  
 
 void BASsleep(int Milliseconds){
    BAS_FUNCTION(BASsleep);
+#ifdef _WIN32
+   Sleep(Milliseconds);
+#else
    timespec I;
    I.tv_sec = Milliseconds / 1000;
    I.tv_nsec = (Milliseconds % 1000) * 1000000;
    nanosleep(&I, NULL);
+#endif
 }
 
